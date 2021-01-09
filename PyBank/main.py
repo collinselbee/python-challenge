2e9c9d3fc214877ddb26ddad13206aabdf64ace0
collinselbee <collins.elbee@gmail.com>
2021-01-09 16:48:09 +0800

    last commit on PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -38,7 +38,7 @@ with open(budget_data, newline = "") as csvfile:
         #Total number of months
         total_months += 1
 
-        #Total net amount of "Profit/Losses over entire period"
+        #Total net amount of Profit/Losses over entire period
         total_pl = total_pl + int(row[1])
 
     #Greatest increase in profits
@@ -51,11 +51,11 @@ with open(budget_data, newline = "") as csvfile:
     worst_index = profits.index(greatest_decrease)
     worst_date = dates[worst_index]
 
-    #Average change in "Profit/Losses between months over entire period"
+    #Average change in Profit/Losses between months over entire period
     avg_change = sum(profits)/len(profits)
     
 
-#Displaying information
+#Displaying information for Financial Analysis with print statements
 print("Financial Analysis")
 print("---------------------")
 print(f"Total Months: {str(total_months)}")
@@ -64,14 +64,15 @@ print(f"Average Change: ${str(round(avg_change,2))}")
 print(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
 print(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
 
-#Exporting to .txt file
-analysis = open("analysis.txt", "w")
-
-line1 = "Financial Analysis"
-line2 = "---------------------"
-line3 = str(f"Total Months: {str(total_months)}")
-line4 = str(f"Total: ${str(total_pl)}")
-line5 = str(f"Average Change: ${str(round(avg_change,2))}")
-line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
-line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
-analysis.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7))
+#Financial Analysis
+#Exporting to .txt file and Writting the new .txt file into Analysis Directory
+from os.path import abspath
+with open ('/Users/bintucolley/Desktop/python-challenge/PyBank/Analysis/output.txt', mode = 'w') as analysis:
+    line1 = "Financial Analysis"
+    line2 = "---------------------"
+    line3 = str(f"Total Months: {str(total_months)}")
+    line4 = str(f"Total: ${str(total_pl)}")
+    line5 = str(f"Average Change: ${str(round(avg_change,2))}")
+    line6 = str(f"Greatest Increase in Profits: {greatest_date} (${str(greatest_increase)})")
+    line7 = str(f"Greatest Decrease in Profits: {worst_date} (${str(greatest_decrease)})")
+    print(analysis.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7)))
